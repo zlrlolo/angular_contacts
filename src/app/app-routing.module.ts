ac2990e9f40e457b4b27b1ee820ac6c5167f49b2
zlrlolo <1139940527@qq.com>
2022-08-24 19:46:23 +0800

    初始

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ContactNewComponent} from "./contact-new/contact-new.component";
+import {LayoutComponent} from "./layout/layout.component";
+import {ContactListComponent} from "./contact-list/contact-list.component";
+import {SigninComponent} from "./signin/signin.component";
+import {SignupComponent} from "./signup/signup.component";
+import {ContactEditComponent} from "./contact-edit/contact-edit.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/contacts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent
+  },
+  {
+    path: 'contacts',
+    component: LayoutComponent,
+    children: [{
+      path: '',
+      component: ContactListComponent
+    },{
+      path:'new',
+      component:ContactNewComponent
+    },
+      {
+        path:'edit',
+        component:ContactEditComponent
+      }]
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
